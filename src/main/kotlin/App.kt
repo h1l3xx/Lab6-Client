efa8ab54cac5e6f628b54dc6fd7c6310fcea1f7a
h1l3xx <alksmlkv@gmail.com>
2023-04-21 13:19:38 +0300

    First commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,4 @@
+fun main(){
+    val client = Client()
+    client.main()
+}
---
+++ src/main/kotlin/Client.kt
@@ -0,0 +1,20 @@
+import java.io.IOException
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.util.*
+
+class Client {
+    fun main() {
+        val sender = Sender("localhost", 1050)
+        val message = "Hello"
+
+
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                sender.sendMessage(message)
+            }
+        }, 1000, 1000)
+    }
+}
