a4c7d84a387514a090e9b7c1f124aea68e9f9e02
Artem622 <artemiiko2014@mail.ru>
2023-04-26 22:58:43 +0300

    communicate with server

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,31 +1,19 @@
+import command.CommandList
 import handkers.*
 import printers.UPrinter
 
 val iteration = Iteration()
+val tttt = CommandList()
 fun main(){
 
-//    val send = SenderProgram()
-//    val answer = ReceiverProgram()
-//    val receiver = answer.startReceiver()
-//    val uPrinter = UPrinter()
-//    var test = true
-////    val sc = Scanner(System.`in`)
-////        test.mailing(sc.nextLine())
-//    send.mailing("Доброго вечера товарищи")
-//    while (test){
-//        uPrinter.print { answer.main(receiver) }
-//        test = false
-//    }
-//    if (test==false){
-//        send.mailing("Доброго вечера товарищи два два два два два два два два два два два два два два два два")
-//        test = true
-//    }
-//    while (test){
-//        uPrinter.print { answer.main(receiver) }
-////        test = false
-//    }
+
         iteration.oneIteration("pizda")
+//        tttt.loop()
         println(desir())
+        for(i in 0.. desir().size-1){
+                println(desir()[i]["name"])
+        }
         iteration.othersIteration("zalupa")
+
 
 }
---
+++ src/main/kotlin/command/CommandList.kt
@@ -0,0 +1,22 @@
+package command
+
+import handkers.desir
+
+class CommandList {
+//    private var commandList  : HashMap<String, String> = HashMap()
+//    var test = desir()
+//    fun loop(){
+//        for(i in 0.. test.size){
+//            println(test[i])
+//        }
+//    }
+//    fun manage(name : String, arguments : List<String>){
+//        if (checkArgumentsInfo(name, arguments)){
+//            val variables = commandList[name]!!.argContract(arguments)
+//            val result = commandList[name]!!.comply(variables)
+//            if (result.getBool()){
+//                uPrinter.print { result.getMessage() }
+//            }
+//        }
+//    }
+}
